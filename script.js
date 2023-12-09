9d73608dc0533665e7d4f0841b367ee6c978aa2e
sebswit <sebastian.switakowski@gmail.com>
2023-12-09 16:46:30 +0000

    fixed HTML added time and loop in js

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 
 //Display current date and time
 // Import for dayjs library
+$(document).ready(function () {
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm A"));
+
+
+    //Create time blocks
+    var timeBlock = $(".time-block");
+    var hour = $(".hour-display");
+    var textArea = $(".description");
+    var saveBtn = $(".saveBtn");
+
+    
+
+    //check if time block is in the past, present or future
+    for (var i = 0; i < timeBlock.length; i++) {
+        
+        if (dayjs().hour() > timeBlock[i].id) {
+            textArea[i].classList.add("past");
+        } else if (dayjs().hour() < timeBlock[i].id) {
+            textArea[i].classList.add("future");
+        } else {
+            textArea[i].classList.add("present");
+        }
+    }
+}); 
+
+
 //Create time blocks
 // use loop to create time blocks
 //check if time block is in the past, present or future
