b09094c20122260bf30f79ddeae7409a44eb41dd
sebswit <sebastian.switakowski@gmail.com>
2023-12-09 16:09:53 +0000

    pseudo code

---
+++ script.js
@@ -0,0 +1,12 @@
+//Pseudo code
+
+//Display current date and time
+// Import for dayjs library
+//Create time blocks
+// use loop to create time blocks
+//check if time block is in the past, present or future
+//load saved events from local storage
+//save events to local storage
+
+// create time blocks
+//Save button click event
